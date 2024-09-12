00857fb0c39977ca65d0d1e9793f9df006b2aa34
Daniel Alves <daniel.alvesmoreira1@gmail.com>
2024-09-11 23:51:16 -0300

    deletar metas

--- index.js
+++ index.js
@@ -107,6 +107,40 @@ const metasAbertas = async () => {
 
 }
 
+
+const deletarMetas = async () => {
+  const metasDesmarcadas = metas.map((meta) => {
+     
+      return {value: meta.value, checked: false }
+    
+    })
+    
+
+    const itemsADeletar = await checkbox({
+      message: "Selecione item para deletar",
+      choices: [...metasDesmarcadas],
+      instructions: false 
+    })
+
+    if(itemsADeletar.length == 0){
+        console.log("Nenhum item para deletar!")
+        return
+
+    }
+
+
+    itemsADeletar.forEach((item) => {
+        metas = metas.filter((meta) => {
+            return meta.value != item
+        })
+
+    })
+
+    console.log("Meta(s) deletada(s) com sucesso!")
+
+
+}
+
 const start = async () =>
 {
 
@@ -132,6 +166,10 @@ const start = async () =>
               value: "abertas"
             },
             {
+              name: "Deletar Metas",
+              value: "deletar"
+            },
+            {
               name: "Sair",
               value: "sair"
             },
@@ -153,6 +191,9 @@ const start = async () =>
           case "abertas":
           await metasAbertas()
           break  
+          case "deletar":
+            await deletarMetas()
+            break  
         case "sair":
           console.log("Até a próxima")
           return  
